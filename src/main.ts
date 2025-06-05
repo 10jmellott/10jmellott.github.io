686ab436c5f7863b475bc5d22fc28759b4da94da
Joshua Mellott-Lillie <10jmellott@gmail.com>
2025-06-04 21:48:34 -0400

    Lazy load pages

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,13 @@ import { createApp } from 'vue';
 import './styles/main.css';
 import App from './App.vue';
 import Home from './pages/Home.vue';
-import Projects from './pages/Projects.vue';
-import Experience from './pages/Experience.vue';
 import { createRouter, createWebHashHistory } from 'vue-router';
-import Me from './pages/Me.vue';
 
 const routes = [
 	{ path: '/', component: Home },
-	{ path: '/me', component: Me },
-	{ path: '/projects', component: Projects },
-	{ path: '/experience', component: Experience },
+	{ path: '/me', component: () => import('./pages/Me.vue') },
+	{ path: '/projects', component: () => import('./pages/Projects.vue') },
+	{ path: '/experience', component: () => import('./pages/Experience.vue') },
 ];
 
 const router = createRouter({
